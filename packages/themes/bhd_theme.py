b1e27da3ab4c6f9b127fe84a096d2276c0232182
jlw4049 <jlw_4049@hotmail.com>
2022-09-07 19:33:51 -0400

    convert theme to a class in order to call it when I need it

--- packages/themes/bhd_theme.py
+++ packages/themes/bhd_theme.py
@@ -1,44 +1,52 @@
-custom_button_colors = {
-    "foreground": "white",
-    "background": "#23272A",
-    "activeforeground": "#3498db",
-    "activebackground": "#23272A",
-    "disabledforeground": "dark grey"
-}
+class BHDTheme:
+    def __init__(self):
+        self.custom_button_colors = {
+            "foreground": "white",
+            "background": "#23272A",
+            "activeforeground": "#3498db",
+            "activebackground": "#23272A",
+            "disabledforeground": "dark grey",
+        }
 
-custom_entry_colors = {
-    "foreground": "white",
-    "background": "#565656",
-    "disabledforeground": "white",
-    "disabledbackground": "#565656",
-}
+        self.custom_entry_colors = {
+            "foreground": "white",
+            "background": "#565656",
+            "disabledforeground": "white",
+            "disabledbackground": "#565656",
+        }
 
-custom_label_frame_colors = {"foreground": "#3498db", "background": "#363636"}
+        self.custom_label_frame_colors = {
+            "foreground": "#3498db",
+            "background": "#363636",
+        }
 
-custom_frame_bg_colors = {
-    "background": "#363636",
-    "highlightcolor": "white",
-    "specialbg": "#434547",
-}
+        self.custom_frame_bg_colors = {
+            "background": "#363636",
+            "highlightcolor": "white",
+            "specialbg": "#434547",
+        }
 
-custom_label_colors = {"foreground": "white", "background": "#363636"}
+        self.custom_label_colors = {"foreground": "white", "background": "#363636"}
 
-custom_window_bg_color = "#363636"
+        self.custom_window_bg_color = "#363636"
 
-custom_scrolled_text_widget_color = {"foreground": "white", "background": "#565656"}
+        self.custom_scrolled_text_widget_color = {
+            "foreground": "white",
+            "background": "#565656",
+        }
 
-custom_listbox_color = {
-    "foreground": "white",
-    "background": "#565656",
-    "selectbackground": "#565656",
-    "selectforeground": "#3498db",
-}
+        self.custom_listbox_color = {
+            "foreground": "white",
+            "background": "#565656",
+            "selectbackground": "#565656",
+            "selectforeground": "#3498db",
+        }
 
-custom_spinbox_color = {
-    "foreground": "white",
-    "background": "#23272A",
-    "buttonbackground": "black",
-    "readonlybackground": "#23272A",
-}
+        self.custom_spinbox_color = {
+            "foreground": "white",
+            "background": "#23272A",
+            "buttonbackground": "black",
+            "readonlybackground": "#23272A",
+        }
 
-custom_text_color = {"background": "#434547", "foreground": "white"}
+        self.custom_text_color = {"background": "#434547", "foreground": "white"}
