2fd67c76eee1074183837c8624dd8ca05ad4cc90
jlw4049 <jlw_4049@hotmail.com>
2022-09-07 19:34:20 -0400

    Currently only system default/bhd theme works still. This will be more compact and more effecient

--- packages/themes/theme_control.py
+++ packages/themes/theme_control.py
@@ -33,16 +33,22 @@ class OpenTheme:
             self.custom_spinbox_color = self.colors.custom_spinbox_color
             self.custom_text_color = self.colors.custom_text_color
 
-        elif self.config_parser["theme"]["selected_theme"] == "bhd_theme":
-            self.custom_window_bg_color = bhd_theme.custom_window_bg_color
-            self.custom_button_colors = bhd_theme.custom_button_colors
-            self.custom_entry_colors = bhd_theme.custom_entry_colors
-            self.custom_label_frame_colors = bhd_theme.custom_label_frame_colors
-            self.custom_frame_bg_colors = bhd_theme.custom_frame_bg_colors
-            self.custom_label_colors = bhd_theme.custom_label_colors
-            self.custom_scrolled_text_widget_color = (
-                bhd_theme.custom_scrolled_text_widget_color
-            )
-            self.custom_listbox_color = bhd_theme.custom_listbox_color
-            self.custom_spinbox_color = bhd_theme.custom_spinbox_color
-            self.custom_text_color = bhd_theme.custom_text_color
+        else:
+            if self.config_parser["theme"]["selected_theme"] == "bhd_theme":
+                values = bhd_theme.BHDTheme()
+
+            self.__theme(values)
+
+    def __theme(self, values):
+        self.custom_window_bg_color = values.custom_window_bg_color
+        self.custom_button_colors = values.custom_button_colors
+        self.custom_entry_colors = values.custom_entry_colors
+        self.custom_label_frame_colors = values.custom_label_frame_colors
+        self.custom_frame_bg_colors = values.custom_frame_bg_colors
+        self.custom_label_colors = values.custom_label_colors
+        self.custom_scrolled_text_widget_color = (
+            values.custom_scrolled_text_widget_color
+        )
+        self.custom_listbox_color = values.custom_listbox_color
+        self.custom_spinbox_color = values.custom_spinbox_color
+        self.custom_text_color = values.custom_text_color
