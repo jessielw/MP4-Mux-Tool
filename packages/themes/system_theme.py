877714db60e7ad931af958ccb85eb8dbb38e4709
jlw4049 <jlw_4049@hotmail.com>
2022-09-07 21:54:47 -0400

    Temp widgets are destroyed after use

--- packages/themes/system_theme.py
+++ packages/themes/system_theme.py
@@ -12,7 +12,10 @@ from tkinter import (
 
 
 class SystemTheme:
-    """creates temporary widgets that never gets displayed to extract their default color/theme"""
+    """
+    Creates temporary widgets that never gets displayed to extract their default color/theme
+    After the values are extracted the widgets are immediately destroyed
+    """
 
     def __init__(self, main_gui):
         """create mp4 window variable and run the system theme method"""
@@ -33,6 +36,7 @@ class SystemTheme:
             "activebackground": temp_btn.cget("activebackground"),
             "disabledforeground": temp_btn.cget("disabledforeground"),
         }
+        temp_btn.destroy()
 
         temp_entry = Entry()
         self.custom_entry_colors = {
@@ -41,12 +45,14 @@ class SystemTheme:
             "disabledforeground": temp_entry.cget("disabledforeground"),
             "disabledbackground": temp_entry.cget("disabledbackground"),
         }
+        temp_entry.destroy()
 
         temp_lbl_frame = LabelFrame()
         self.custom_label_frame_colors = {
             "foreground": temp_lbl_frame.cget("fg"),
             "background": temp_lbl_frame.cget("bg"),
         }
+        temp_lbl_frame.destroy()
 
         temp_frame = Frame()
         self.custom_frame_bg_colors = {
@@ -54,18 +60,21 @@ class SystemTheme:
             "highlightcolor": temp_frame.cget("highlightcolor"),
             "specialbg": temp_frame.cget("bg"),
         }
+        temp_frame.destroy()
 
         temp_label = Label()
         self.custom_label_colors = {
             "foreground": temp_label.cget("fg"),
             "background": temp_label.cget("bg"),
         }
+        temp_label.destroy()
 
         temp_scrolled_text = scrolledtext.ScrolledText()
         self.custom_scrolled_text_widget_color = {
             "foreground": temp_scrolled_text.cget("fg"),
             "background": temp_scrolled_text.cget("bg"),
         }
+        temp_scrolled_text.destroy()
 
         temp_listbox = Listbox()
         self.custom_listbox_color = {
@@ -74,6 +83,7 @@ class SystemTheme:
             "selectbackground": temp_listbox.cget("selectbackground"),
             "selectforeground": temp_listbox.cget("selectforeground"),
         }
+        temp_listbox.destroy()
 
         temp_spinbox = Spinbox()
         self.custom_spinbox_color = {
@@ -82,9 +92,11 @@ class SystemTheme:
             "buttonbackground": temp_spinbox.cget("buttonbackground"),
             "readonlybackground": temp_spinbox.cget("readonlybackground"),
         }
+        temp_spinbox.destroy()
 
         temp_text = Text()
         self.custom_text_color = {
             "background": temp_text.cget("bg"),
             "foreground": temp_text.cget("fg"),
         }
+        temp_text.destroy()
