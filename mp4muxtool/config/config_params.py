5a035bc9d5f16ee687570c3a4a0a6ffd4f5afbd2
jlw4049 <jlw_4049@hotmail.com>
2022-09-22 21:57:06 -0400

    Update config_params.py
    Add ffmpeg path, last saved directory, and mp4 gui/demux window geometry position support

--- mp4muxtool/config/config_params.py
+++ mp4muxtool/config/config_params.py
@@ -14,6 +14,11 @@ if not config.has_section("mkvextract_path"):
 if not config.has_option("mkvextract_path", "path"):
     config.set("mkvextract_path", "path", "")
 
+if not config.has_section("ffmpeg_path"):
+    config.add_section("ffmpeg_path")
+if not config.has_option("ffmpeg_path", "path"):
+    config.set("ffmpeg_path", "path", "")
+
 if not config.has_section("debug_option"):
     config.add_section("debug_option")
 if not config.has_option("debug_option", "option"):
@@ -24,15 +29,27 @@ if not config.has_section("auto_close_progress_window"):
 if not config.has_option("auto_close_progress_window", "option"):
     config.set("auto_close_progress_window", "option", "on")
 
+if not config.has_section("auto_chapter_import"):
+    config.add_section("auto_chapter_import")
+if not config.has_option("auto_chapter_import", "option"):
+    config.set("auto_chapter_import", "option", "on")
+
 if not config.has_section("reset_program_on_start_job"):
     config.add_section("reset_program_on_start_job")
 if not config.has_option("reset_program_on_start_job", "option"):
     config.set("reset_program_on_start_job", "option", "off")
 
-if not config.has_section("auto_chapter_import"):
-    config.add_section("auto_chapter_import")
-if not config.has_option("auto_chapter_import", "option"):
-    config.set("auto_chapter_import", "option", "on")
+if not config.has_section("last_saved_dir"):
+    config.add_section("last_saved_dir")
+if not config.has_option("last_saved_dir", "dir"):
+    config.set("last_saved_dir", "dir", "on")
+
+if not config.has_section("window_positions"):
+    config.add_section("window_positions")
+if not config.has_option("window_positions", "mp4_gui"):
+    config.set("window_positions", "mp4_gui", "")
+if not config.has_option("window_positions", "demux_window"):
+    config.set("window_positions", "demux_window", "")
 
 if not config.has_section("theme"):
     config.add_section("theme")
