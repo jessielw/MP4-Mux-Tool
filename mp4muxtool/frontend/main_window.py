57240fc786862c2996c36a05de5ed37b03aef19e
jlw4049 <jlw_4049@hotmail.com>
2023-11-25 13:22:11 -0500

    feat: added theme loader to load a themes as needed (customizable by the user later) refactor: window is now smaller

--- mp4muxtool/frontend/main_window.py
+++ mp4muxtool/frontend/main_window.py
@@ -1,16 +1,18 @@
 from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, QToolButton, QFrame
 from PySide6.QtGui import QIcon
 
-from mp4muxtool.frontend.custom_widgets.buttons import IconToolButton
+from mp4muxtool.frontend.theme_loader import load_theme
 from mp4muxtool.frontend.button_box import ButtonBox
 from mp4muxtool.frontend.content_box.content_box import ContentBox
 
 class Mp4MuxWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(800, 450)
+        self.resize(600, 400)
         
-        button_box = ButtonBox()
+        theme = load_theme("themes/dark.json").get("theme")
+        
+        button_box = ButtonBox(theme)
         content_box = ContentBox(button_box)
         
         # TODO: alternative solution to define signals AFTER widget creation
