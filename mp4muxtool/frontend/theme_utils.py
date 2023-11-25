2d80b0df26f3aeae674bbae89512eed489b05074
jlw4049 <jlw_4049@hotmail.com>
2023-11-25 13:51:15 -0500

    refactor: renamed theme_loader to theme_utils feat: added a function to convert hex color codes to RGB and return them in a tuple

--- mp4muxtool/frontend/theme_utils.py
+++ mp4muxtool/frontend/theme_utils.py
@@ -6,3 +6,9 @@ def load_theme(file_path):
     with open(file_path, "r") as theme:
         theme_data = json.load(theme)
     return theme_data
+
+
+def hex_to_rgb(hex_color):
+    """Hex color codes to RGB"""
+    hex_color = hex_color.lstrip("#")
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
