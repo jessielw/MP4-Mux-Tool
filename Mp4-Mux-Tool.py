327d3880fcaa826ab49c51617d11499cc6354d89
jlw4049 <jlw_4049@hotmail.com>
2023-11-27 15:59:23 -0500

    refactor: add todo's

--- Mp4-Mux-Tool.py
+++ Mp4-Mux-Tool.py
@@ -5,10 +5,14 @@ from PySide6.QtGui import QFont, QFontDatabase
 from mp4muxtool.frontend.main_window import Mp4MuxWindow
 
 # TODO: We'll upgrade python to 10.5 or higher and remove union everywhere throughout
-        
+# TODO: Since some methods will be the same between content tabs let's do a general class
+# to inherit methods from
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    font_id = QFontDatabase.addApplicationFont("mp4muxtool/frontend/font/OpenSans-Medium.ttf")
+    font_id = QFontDatabase.addApplicationFont(
+        "mp4muxtool/frontend/font/OpenSans-Medium.ttf"
+    )
     font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
     app.setFont(QFont(font_family, 9))
     app.setStyle("Fusion")
