b74f1d0b344c62ef9d6efbeed0c2f6a377d1e8c9
jlw4049 <jlw_4049@hotmail.com>
2023-11-28 13:00:31 -0500

    refactor: move main_window to it's own directory style: styled the background of the main window feat: added a way to toggle the entire ui enabled/disabled via the main window without passing the instance down through all the classes refactor: did some minor tweaks to the layout

---
+++ mp4muxtool/frontend/main_window/main_window.py
@@ -0,0 +1,49 @@
+from PySide6.QtWidgets import (
+    QWidget,
+    QHBoxLayout,
+    QVBoxLayout,
+)
+
+from mp4muxtool.frontend.main_window.state import MainWindowState
+from mp4muxtool.frontend.theme_utils import load_theme
+from mp4muxtool.frontend.button_box import ButtonBox
+from mp4muxtool.frontend.content_box.content_box import ContentBox
+from mp4muxtool.frontend.status_box.status_box import StatusBox
+
+
+class Mp4MuxWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(600, 400)
+
+        self.main_window_state = MainWindowState.get_instance()
+        self.main_window_state.toggle_state.connect(self._toggle_state)
+
+        theme = load_theme("themes/dark.json")
+
+        main_window_theme = theme.get("main-window")
+        self.setStyleSheet(
+            f"Mp4MuxWindow {{background-color: {main_window_theme.get('background')};}}"
+        )
+
+        button_box = ButtonBox(theme)
+
+        content_box = ContentBox(theme, button_box)
+        status_box = StatusBox(theme)
+
+        content_layout = QVBoxLayout()
+        content_layout.addWidget(content_box, 10)
+        content_layout.addWidget(status_box, 1)
+        content_layout.setContentsMargins(2, 0, 0, 0)
+
+        # TODO: alternative solution to define signals AFTER widget creation
+        # content_box.setup_signals(button_box)
+
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.addWidget(button_box, stretch=1)
+        layout.addLayout(content_layout, stretch=6)
+
+    def _toggle_state(self, toggle: bool):
+        self.setEnabled(toggle)
