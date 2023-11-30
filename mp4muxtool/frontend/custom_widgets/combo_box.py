a6e046cc3e0a7f69c5cc3aaf9b9ceb0877cd2bbf
jlw4049 <jlw_4049@hotmail.com>
2023-11-30 13:00:30 -0500

    feat: added a method that will automatically clear/flash red if a user types text that is not in the supported list refactor: cleaned up styles a little. The pop up window that comes up when a user is typing is based off of the OS and can't be controlled, however the combobox/qline is styled to match

--- mp4muxtool/frontend/custom_widgets/combo_box.py
+++ mp4muxtool/frontend/custom_widgets/combo_box.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QComboBox, QCompleter
+from PySide6.QtCore import QTimer
 
 # from PySide6.QtGui import QPalette, QColor
 # from mp4muxtool.frontend.theme_utils import hex_to_rgb
@@ -10,27 +11,30 @@ class CustomComboBox(QComboBox):
     ):
         super().__init__(parent)
 
-        combo_box_theme = theme.get("combo-box")
+        self.timer = None
+
+        self.theme = theme
+        self.combo_box_theme = self.theme.get("combo-box")
         # text_color = hex_to_rgb(combo_box_theme.get("text"))
         # background_color = hex_to_rgb(combo_box_theme.get("background"))
 
-        self.setStyleSheet(
-            f"""
+        self.original_style = f"""
             CustomComboBox {{
-                background-color: {combo_box_theme.get("background")};
-                color: {combo_box_theme.get("text")};
-                border: 1px solid {combo_box_theme.get("border")};
+                background-color: {self.combo_box_theme.get("background")};
+                color: {self.combo_box_theme.get("text")};
+                border: 1px solid {self.combo_box_theme.get("border")};
                 border-top-left-radius: 2px;
                 border-bottom-left-radius: 2px;
             }}
             CustomComboBox:hover {{
-                border-color: {combo_box_theme.get("border-hover")};
+                border-color: {self.combo_box_theme.get("border-hover")};
             }}
             CustomComboBox QAbstractItemView {{
-                color: {combo_box_theme.get("text")};  
-            }}
+                color: {self.combo_box_theme.get("text")};  
+            }}           
         """
-        )
+
+        self.setStyleSheet(self.original_style)
         self.setEditable(True)
         self.setMaxVisibleItems(max_items)
         if not completer:
@@ -39,6 +43,7 @@ class CustomComboBox(QComboBox):
         else:
             self.setInsertPolicy(QComboBox.NoInsert)
             self.completer().setCompletionMode(QCompleter.PopupCompletion)
+            self.lineEdit().editingFinished.connect(self.checkEnteredText)
 
         # Create a custom palette for the combo box
         # combo_palette = QPalette()
@@ -55,3 +60,18 @@ class CustomComboBox(QComboBox):
 
         # Apply the custom palette to the dropdown menu
         # self.view().setPalette(menu_palette)
+
+    def checkEnteredText(self):
+        entered_text = self.currentText()
+        if entered_text not in [self.itemText(i) for i in range(self.count())]:
+            modified_style = self.original_style.replace(
+                f"border: 1px solid {self.combo_box_theme.get('border')};",
+                f"border: 1px solid {self.theme.get('delete-color')};",
+            )
+            self.setStyleSheet(modified_style)
+            if self.timer:
+                self.timer.stop()
+            self.timer = QTimer(self)
+            self.timer.timeout.connect(lambda: self.setStyleSheet(self.original_style))
+            self.timer.start(1000)
+            self.setCurrentIndex(0)
