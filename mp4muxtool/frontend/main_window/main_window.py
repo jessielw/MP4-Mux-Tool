91d81c252549c893c84fb81a4ef8f921043e7aa4
jlw4049 <jlw_4049@hotmail.com>
2023-12-05 13:26:56 -0500

    refactor: theme is now handled in the StyleFactory refactor: the main window now uses the QMainWindow instead of the QWidget. This is important if we want to add a menu bar later if needed plus some other benefits refactor: updated code to look the same as before since using QMainWindow

--- mp4muxtool/frontend/main_window/main_window.py
+++ mp4muxtool/frontend/main_window/main_window.py
@@ -2,31 +2,42 @@ from PySide6.QtWidgets import (
     QWidget,
     QHBoxLayout,
     QVBoxLayout,
+    QMainWindow
 )
 
 from mp4muxtool.frontend.main_window.state import MainWindowState
-from mp4muxtool.frontend.main_window.global_style_sheet import generate_style_sheet
+from mp4muxtool.frontend.styles.styles import StyleFactory
 from mp4muxtool.frontend.theme_utils import load_theme
-from mp4muxtool.frontend.button_box import ButtonBox
+from mp4muxtool.frontend.nav_panel import NavigationalPanel
 from mp4muxtool.frontend.content_box.content_box import ContentBox
 from mp4muxtool.frontend.status_box.status_box import StatusBox
 
 
-class Mp4MuxWindow(QWidget):
+class Mp4MuxWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setObjectName("Mp4MuxWindow")
+        
         self.resize(600, 400)
 
         self.main_window_state = MainWindowState.get_instance()
         self.main_window_state.toggle_state.connect(self._toggle_state)
 
         theme = load_theme("themes/dark.json")
-        self.setStyleSheet(generate_style_sheet(theme))
+        theme_factory = StyleFactory.get_instance()
+        theme_factory.update_theme(theme)
+        self.setStyleSheet(theme_factory.get_global_theme())
 
-        button_box = ButtonBox(theme)
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)    
 
-        content_box = ContentBox(theme, button_box)
-        status_box = StatusBox(theme)
+        main_layout = QHBoxLayout(central_widget)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
+
+        nav_panel = NavigationalPanel()
+        content_box = ContentBox(nav_panel)
+        status_box = StatusBox()
 
         content_layout = QVBoxLayout()
         content_layout.addWidget(content_box, 10)
@@ -36,11 +47,8 @@ class Mp4MuxWindow(QWidget):
         # TODO: alternative solution to define signals AFTER widget creation
         # content_box.setup_signals(button_box)
 
-        layout = QHBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setSpacing(0)
-        layout.addWidget(button_box, stretch=1)
-        layout.addLayout(content_layout, stretch=6)
+        main_layout.addWidget(nav_panel, stretch=1)
+        main_layout.addLayout(content_layout, stretch=6)
 
     def _toggle_state(self, toggle: bool):
         self.setEnabled(toggle)
