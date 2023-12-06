4b4114a6889985be035c096ba533e05808ba40da
jlw4049 <jlw_4049@hotmail.com>
2023-12-05 20:01:10 -0500

    style: format style

--- mp4muxtool/frontend/styles/styles.py
+++ mp4muxtool/frontend/styles/styles.py
@@ -7,14 +7,15 @@ from mp4muxtool.frontend.styles.status_box import theme_status_box
 from mp4muxtool.frontend.styles.loading_bar import theme_loading_bar
 from mp4muxtool.frontend.styles.custom_combo_box import theme_custom_combo_box
 
-class StyleFactory():
+
+class StyleFactory:
     _instance = None
     theme = None
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(StyleFactory, cls).__new__(cls)
-            
+
         return cls._instance
 
     @classmethod
@@ -22,30 +23,30 @@ class StyleFactory():
         if cls._instance is None:
             cls._instance = StyleFactory()
         return cls._instance
-    
+
     def update_theme(cls, theme):
         cls.theme = theme
-        
+
     def get_global_theme(cls):
         return theme_global(cls.theme)
-         
+
     def get_nav_theme(cls):
         return theme_nav_panel(cls.theme)
-    
+
     def get_layout_theme(cls):
         return theme_layout_content(cls.theme)
-    
+
     def get_video_content_theme(cls):
         return theme_video_content(cls.theme)
-    
+
     def get_audio_content_theme(cls):
-        return theme_audio_content(cls.theme)    
-    
+        return theme_audio_content(cls.theme)
+
     def get_status_box_theme(cls):
         return theme_status_box(cls.theme)
-    
+
     def get_loading_bar_theme(cls):
-        return theme_loading_bar(cls.theme)    
-    
+        return theme_loading_bar(cls.theme)
+
     def get_custom_combo_box_theme(cls):
         return theme_custom_combo_box(cls.theme)
