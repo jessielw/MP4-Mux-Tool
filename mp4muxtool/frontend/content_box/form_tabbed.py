995cab6934c22856edb0a3bd3ef209b5d25d0f42
jlw4049 <jlw_4049@hotmail.com>
2023-12-07 12:28:56 -0500

    feat: create a form tabbed module, this allows us to dynamically use this in both the subtitle/audio tabs since there can be multiple of these tracks

---
+++ mp4muxtool/frontend/content_box/form_tabbed.py
@@ -0,0 +1,89 @@
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QVBoxLayout, QWidget, QToolButton, QTabWidget
+
+from mp4muxtool.frontend.styles.styles import StyleFactory
+from mp4muxtool.frontend.content_box.form_layout import LayoutContent
+
+
+class ContentTabbed(QTabWidget):
+    def __init__(self, object_name: str, backend: object, name: str, layout_object_name: str, extensions: list, enable_delay: bool):
+        super().__init__()
+        self.setObjectName(object_name)
+        # self.setStyleSheet(StyleFactory.get_instance().get_audio_content_theme())
+        
+        # self.tab_data = []
+        
+        self.backend = backend
+        self.name = name
+        self.layout_object_name = layout_object_name
+        self.extensions = extensions
+        self.enable_delay = enable_delay
+        
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(2, 2, 2, 2)
+        
+        self.setTabsClosable(True)
+        self.setMovable(True)
+        self.tabCloseRequested.connect(self._close_tab)
+        # self.currentChanged.connect(self._update_tabs)     
+        self.tabBar().tabMoved.connect(self._update_tabs)
+        
+        self._add_new_tab()    
+                
+        
+    def _add_new_tab(self):
+        new_tab = QWidget()
+        new_tab.setObjectName("tabArea")
+        # new_tab.setStyleSheet("background-color: #2f3132;")
+        
+        # audio_content.setObjectName("AudioLayout")
+        # main_area = DummyAudioClass(self.count())
+        # audio_content = AudioContent(self.theme)
+        audio_content = LayoutContent(self.backend, self.name, self.layout_object_name, self.extensions, 10, self.enable_delay)
+        # self.tab_data.append(main_area)
+        audio_content.setObjectName("tabArea")
+        
+        tab_layout = QVBoxLayout(new_tab)
+        tab_layout.setContentsMargins(0, 0, 0, 0)        
+        tab_layout.addWidget(audio_content, 6)
+        
+        new_tab_button = LayoutContent._build_icon_buttons(QToolButton, "add_circle.svg", "addNewTab")
+        new_tab_button.setStyleSheet(StyleFactory.get_instance().get_audio_content_theme())   
+        new_tab_button.setFixedHeight(28)
+        new_tab_button.setToolTip("Add new subtitle track")
+        new_tab_button.clicked.connect(self._add_new_tab)   
+
+        # Add your button or other widgets to the layout
+        tab_layout.addWidget(new_tab_button, 1, Qt.AlignmentFlag.AlignRight)
+
+        # Add the new tab to the QTabWidget
+        self.addTab(new_tab, f"Track {self.count() + 1}")
+        
+        self.setCurrentIndex(self.count() - 1)       
+    
+    def _close_tab(self, index):
+        if self.count() != 1:
+            self.removeTab(index)
+            self._update_tabs()
+            
+    # def _tabs_moved(self, from_index, to_index):
+    #     # Update the track numbers based on the tab movement
+    #     moved_tab = self.tab_data.pop(from_index)
+    #     self.tab_data.insert(to_index, moved_tab)
+
+    #     # Print the current state
+    #     print([(f"Current {x.track}", f"OG {x.og_track}") for x in self.tab_data])
+                  
+    def _update_tabs(self):
+        for tab_index in range(self.count()):
+            self.setTabText(tab_index, f"Track {tab_index + 1}")  
+            
+            
+        # for tab in self.count():
+        #     print(tab)       
+                 
+            # Access the DummyAudioClass instance for the current tab
+            # dummy_instance = self.widget(tab_index).findChild(DummyAudioClass)
+            # if dummy_instance:
+            #     print(f"Tab {tab_index + 1} - Track: {dummy_instance.track}, OG Track: {dummy_instance.og_track}")            
+                              
